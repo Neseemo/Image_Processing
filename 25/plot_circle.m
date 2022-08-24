af0d3d50e33a986caa2f2f37e53d9df0c50a7145
Sim1 <84195928+Neseemo@users.noreply.github.com>
2022-08-24 10:19:24 +0200

    Delete 25 directory

--- 25/plot_circle.m
+++
@@ -1,5 +0,0 @@
-function  plot_circle(x_c,y_c,r, col)
-th = 0:pi/50:2*pi;
-xunit = r * cos(th) + x_c;
-yunit = r * sin(th) + y_c;
-plot(xunit, yunit, 'Color', col);
--- 25/polar_coord.m
+++
@@ -1,5 +0,0 @@
-function [angle, dist_from_origin] = polar_coord(P)
-dist_from_origin = vecnorm(P);
-angle = zeros(1,size(P,2));
-angle(P(1,:)>0) = atan(P(2,P(1,:)>0)./P(1,P(1,:)>0));
-angle(P(1,:)<=0) = atan(P(2,P(1,:)<=0)./P(1,P(1,:)<=0));
